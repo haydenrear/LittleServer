7f8b437dd55d7d988b8bf5e857861fa9fad68990
Hayden Rear <hayden.rear@gmail.com>
2021-02-06 17:56:26 -0800

    Initial commit

--- src/main/java/com/ee/okay/LittleServer.java
+++ src/main/java/com/ee/okay/LittleServer.java
@@ -7,13 +7,13 @@ import javax.servlet.ServletException;
 
 public class LittleServer {
 
-    private String hello = "hello world";
-
     public static void main(String [] args) throws LifecycleException, ServletException {
 
+        String userDir =System.getProperty("user.dir");
+        System.out.println(userDir);
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8000);
-        tomcat.addWebapp("/", "C:\\Users\\hayde\\IdeaProjects\\ee\\src\\main\\resources");
+        tomcat.addWebapp("/", userDir+"\\src\\main\\resources");
         tomcat.start();
         tomcat.getServer().await();
 
